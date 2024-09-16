ab2b6e43e9f422f219beabab29768bd840c1a43f
Märt <mart.puusaag11@gmail.com>
2024-09-16 10:00:27 +0300

    favicon, login page

--- static/graphql.js
+++ static/graphql.js
@@ -4,3 +4,5 @@ const kjGraphQL = "https://01.kood.tech/api/graphql-engine/v1/graphql"
 
 // Main JS //
 
+const loginButton = document.getElementById('loginButton')
+const errorFlair = document.getElementById('errorFlair')
