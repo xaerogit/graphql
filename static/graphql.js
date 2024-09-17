2cf0f0c30598861323925370b11abf3ef46b7aca
Märt <mart.puusaag11@gmail.com>
2024-09-17 12:14:26 +0300

    Additional outlining.

--- static/graphql.js
+++ static/graphql.js
@@ -1,17 +1,30 @@
 // API Endpoints //
-const kjSignInEndpoint = "https://01.kood.tech/api/auth/signin"
-const kjGraphQL = "https://01.kood.tech/api/graphql-engine/v1/graphql"
+const kjSignInEndpoint = "https://01.kood.tech/api/auth/signin";
+const kjGraphQL = "https://01.kood.tech/api/graphql-engine/v1/graphql";
 
 // Main JS //
+const loginButton = document.getElementById('loginButton');
+const errorFlair = document.getElementById('errorFlair');
+const loginPage = document.getElementById('login-div');
+const logoutButton = document.getElementById('logoutButton');
+const loginUsername = document.getElementById('loginUsername');
 
-const loginButton = document.getElementById('loginButton')
-const errorFlair = document.getElementById('errorFlair')
-const loginPage = document.getElementById('login-div')
+loginButton.addEventListener("click"
+  
+)
+logoutButton.addEventListener("click"
 
-function getToken(){
+)
 
-}
-
-function main(){
-    
+const testQuery = async(token)=> { `
+    query {
+        result {
+          id
+          user {
+            id
+            login
+          }
+        }
+      }
+      `
 }
