164e77ccdbc95b449a9e4d38851a8beeb936d95a
Märt <mart.puusaag11@gmail.com>
2024-11-02 10:47:28 +0200

    Newer version

--- static/graphql.mjs
+++ static/graphql.mjs
@@ -1,6 +1,7 @@
+import { fetchQuery } from "./queryHelper.js";
+
 // API Endpoints //
 const kjSignInEndpoint = "https://01.kood.tech/api/auth/signin";
-const kjGraphQL = "https://01.kood.tech/api/graphql-engine/v1/graphql";
 
 // Main JS //
 const removeToken = () => localStorage.removeItem("jwt");
@@ -11,7 +12,7 @@ const saveToken = (token) => {
     console.debug("Token saved to localStorage:", token); 
   }
 }
-const getToken = () => {
+export const getToken = () => {
   const token = localStorage.getItem("jwt");
   console.debug("Retrieved token:", token)
   return token
@@ -34,6 +35,8 @@ const login = async (username, password) => {
       const jwtToken = data
       saveToken(jwtToken)
       getUserQuery(jwtToken)
+      getXPQuery(jwtToken)
+      getSkillsQuery(jwtToken)
     }
   })
   .catch(error => {
@@ -41,29 +44,29 @@ const login = async (username, password) => {
   })
 }
 
-function fetchQuery(query) {
-  return fetch(kjGraphQL, {
-    method: "POST",
-    headers: {
-      "Authorization": `Bearer ${getToken()}`,
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ query: query })
-  })
-  .then(res => {
-    console.debug("Fetch response status:", res.status);
-    return res.json();
-  })
-  .then(data => {
-    console.debug("Data received from fetchQuery:", data);
-    return data;
-  })
-  .catch(error => {
-    console.error("Error in fetchQuery:", error);
-  });
-}
+// function fetchQuery(query) {
+//   return fetch(kjGraphQL, {
+//     method: "POST",
+//     headers: {
+//       "Authorization": `Bearer ${getToken()}`,
+//       "Content-Type": "application/json"
+//     },
+//     body: JSON.stringify({ query: query })
+//   })
+//   .then(res => {
+//     console.debug("Fetch response status:", res.status);
+//     return res.json();
+//   })
+//   .then(data => {
+//     console.debug("Data received from fetchQuery:", data);
+//     return data;
+//   })
+//   .catch(error => {
+//     console.error("Error in fetchQuery:", error);
+//   });
+// }
 
-const getUserQuery = () => {
+function getUserQuery() {
   fetchQuery(`
     {
     user {
@@ -75,7 +78,7 @@ const getUserQuery = () => {
       auditRatio
     }
   }`).then(data => {
-      console.debug("GraphQL Query Result:", data); // Log the final result
+      console.debug("GraphQL User Query Result:", data);
       const userData = data.data.user[0]
       if(document.getElementById("userInfo")) {
         document.getElementById("userInfo").innerHTML = `
@@ -92,13 +95,54 @@ const getUserQuery = () => {
     });
 }
 
-// const skillsInfo = (data) => {
-  // const skillsData = data.skills[0]
-// }
+function getXPQuery() {
+  fetchQuery(`
+    {
+  xp: transaction(
+    where: { _and: [{ type: { _eq: "xp" } }, { path: { _nlike: "%piscine%" } }] }
+  ) {
+    type
+    path
+    createdAt
+    amount
+    object {
+      name
+    }
+  }
+}`).then(data => {
+  console.debug("GraphQL XP Query Result:", data);
+  const xpData = data.data.xp[0]
+  if(document.getElementById("xpGraph")){
+    document.getElementById("xpGraph").innerHTML =`
+    <p>${xpData}</p>`
+  }
+  }).catch(error => {
+    console.error("Error in getQuery:", error);
+  });
+}
 
-// const xpInfo = (data) => {
-  
-// }
+function getSkillsQuery() {
+  fetchQuery(`
+    {
+  skills: transaction(
+    limit: 100
+    offset: 0
+    where: { _and: [{ type: { _ilike: "%skill%" } }] }
+  ) {
+    type
+    amount
+  }
+}`).then(data => {
+  console.debug("GraphQL Skills Query Result:", data);
+  const skillsData = data.data.skills[0]
+  if(document.getElementById("skillsChart")) {
+    document.getElementById("skillsChart").innerHTML = `
+    <p>${skillsData}</p>`
+  }
+  }).catch(error => {
+    console.error("Error in getQuery:", error);
+  });
+}
 
 if (document.getElementById("loginForm")) {
   document.getElementById("loginForm").addEventListener("submit", async (e) => {
